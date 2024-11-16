1e573c603fe2aaafe6e5a90cd3c28b24b3b34cdd
Shinyila <shinyila14@gmail.com>
2024-11-17 04:55:51 +0530

    zxcvbnm

--- app/dashboard/billing/page.tsx
+++ app/dashboard/billing/page.tsx
@@ -112,7 +112,7 @@ function billing() {
             <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
           </svg>
 
-          <span className="text-gray-700"> 50+ Content Templates </span>
+          <span className="text-gray-700"> 20+ Content Templates </span>
         </li>
 
         <li className="flex items-center gap-1">
@@ -198,7 +198,7 @@ function billing() {
             <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
           </svg>
 
-          <span className="text-gray-700"> 50+ Template Access </span>
+          <span className="text-gray-700"> 20+ Template Access </span>
         </li>
 
         <li className="flex items-center gap-1">
